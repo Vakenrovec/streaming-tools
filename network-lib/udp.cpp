5956ba6b89ead145480f4b19f3cddacc64031cd9
LAPTOP-LAQS1OGD\Vakenrovec <andrey159159@gmail.com>
2021-04-11 22:53:09 +0300

    Add network-lib

---
+++ network-lib/udp.cpp
@@ -0,0 +1,5 @@
+#include "udp.h"
+
+const int UDP::MTU = 1500;
+const int UDP::IPv4HeaderSize = 20;
+const int UDP::UDPHeaderSize = 8;
---
+++ network-lib/udp.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class UDP
+{
+public:
+    static const int MTU;
+    static const int IPv4HeaderSize;
+    static const int UDPHeaderSize;
+};
