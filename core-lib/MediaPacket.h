bbf795310282381e9dd8b96ae01470e3cff32986
LAPTOP-LAQS1OGD\Vakenrovec <andrey159159@gmail.com>
2021-04-11 22:51:34 +0300

    Add media packet

---
+++ core-lib/MediaPacket.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include <memory>
+
+typedef enum PacketType: std::uint8_t {
+    UNKNOWN = 0,
+    AUDIO = 1,
+    VIDEO_RTP = 2,
+} packet_type_t;
+
+#pragma pack(push, 1)
+
+typedef struct {
+   PacketType type;         /* payload type */
+   std::uint64_t ts;        /* timestamp */
+   std::uint32_t size;      /* the size of the payload data */
+} media_pkt_header_t;
+
+#pragma pack(pop)
+
+typedef struct {
+    media_pkt_header_t header;
+    std::vector<std::uint8_t> data;
+} media_packet_t;
+
+#define MEDIA_PACKET_SIZE_BYTES sizeof(media_pkt_header_t)
+
+using media_packet_ptr = std::shared_ptr<media_packet_t>;
