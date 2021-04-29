81b73c1b35e9ecf9c2815e1d2bb14c2364954e16
LAPTOP-LAQS1OGD\Vakenrovec <andrey159159@gmail.com>
2021-04-29 21:08:26 +0300

    Add udp_packet

---
+++ network-lib/UDPPacket.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Network.h>
+#include <cstdint>
+#include <memory>
+
+enum class udp_packet_type_t: std::uint8_t {
+    UNKNOWN = 0,
+    RTP = 1,
+    AUDIO = 2,
+};
+
+#pragma pack(push, 1)
+
+typedef struct {
+    udp_packet_type_t type;          // payload type 
+    std::uint64_t ts;                // timestamp
+    std::uint32_t size;              // the size of the payload data 
+} udp_pkt_header_t;
+
+#pragma pack(pop)
+
+constexpr inline std::uint32_t UDPPacketDataSize = Network::MaxUdpPacketSize - sizeof(udp_pkt_header_t);
+
+typedef struct {
+    udp_pkt_header_t header;
+    std::uint8_t data[UDPPacketDataSize];               // rtp packet
+} udp_packet_t;
+
+using udp_packet_ptr = std::shared_ptr<udp_packet_t>;
