52f6911f9522ee17c6f059f3134849f9cfc5b264
LAPTOP-LAQS1OGD\Vakenrovec <andrey159159@gmail.com>
2021-05-11 17:49:52 +0300

    Add delay processor

---
+++ core-lib/utility/DelayDataProcessor.h
@@ -0,0 +1,24 @@
+#pragma once 
+
+#include "DataProcessor.h"
+#include <thread>
+#include <chrono>
+#include <cstdint>
+
+class DelayDataProcessor: public DataProcessor
+{
+public:
+    void Process(const media_packet_ptr& pkt) override
+    {
+        if (m_prevTs != 0)
+        {
+            m_delay = pkt->header.ts - m_prevTs;
+            std::this_thread::sleep_for(std::chrono::milliseconds(m_delay));
+        }
+        m_prevTs = pkt->header.ts;
+        DataProcessor::Process(pkt);
+    }
+
+private:
+    uint64_t m_prevTs = 0, m_delay = 0;
+};
