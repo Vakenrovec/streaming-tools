2f1e2a85725c25cf94ef33df44c0ad04faab5989
LAPTOP-LAQS1OGD\Vakenrovec <andrey159159@gmail.com>
2021-05-19 23:15:09 +0300

    Add horizontal flip to display

--- core-lib/video/VideoDisplayProcessor.cpp
+++ core-lib/video/VideoDisplayProcessor.cpp
@@ -4,21 +4,21 @@
 #include <chrono>
 
 VideoDisplayProcessor::VideoDisplayProcessor(int width, int height)
-: m_screen(nullptr)
+: m_window(nullptr)
 , m_renderer(nullptr)
 , m_texture(nullptr)
 , m_width(width)
 , m_height(height)
 {
-    m_rect.x = 0;
-    m_rect.y = 0;
-    m_rect.w = m_width;
-    m_rect.h = m_height;
+    m_srcRect.x = 0;
+    m_srcRect.y = 0;
+    m_srcRect.w = m_width;
+    m_srcRect.h = m_height;
 }
 
 void VideoDisplayProcessor::Init()
 {
-    m_screen = SDL_CreateWindow(
+    m_window = SDL_CreateWindow(
         "SDL Media Player",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
@@ -26,13 +26,13 @@ void VideoDisplayProcessor::Init()
         m_height,
         SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI
     );
-    if (!m_screen)
+    if (!m_window)
     {
         LOG_EX_WARN_WITH_CONTEXT("SDL: could not create window");
         return;
     }
     SDL_GL_SetSwapInterval(1);
-    m_renderer = SDL_CreateRenderer(m_screen, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
+    m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
     m_texture = SDL_CreateTexture(
         m_renderer,
         SDL_PIXELFORMAT_YV12,
@@ -54,9 +54,9 @@ void VideoDisplayProcessor::Destroy()
         SDL_DestroyRenderer(m_renderer);
         m_renderer = nullptr;
     }
-    if (m_screen) {
-        SDL_DestroyWindow(m_screen);
-        m_screen = nullptr;
+    if (m_window) {
+        SDL_DestroyWindow(m_window);
+        m_window = nullptr;
     }
 
     DataProcessor::Destroy();
@@ -78,26 +78,27 @@ void VideoDisplayProcessor::Process(const media_packet_ptr& pkt)
 void VideoDisplayProcessor::Display(const media_packet_ptr& pkt)
 {
     int ret = SDL_UpdateYUVTexture(
-        m_texture,                                      // the texture to update
-        &m_rect,                                        // a pointer to the rectangle of pixels to update, or NULL to update the entire texture
-        pkt->data,                                      // the raw pixel data for the Y plane
-        m_width,                                        // the number of bytes between rows of pixel data for the Y plane
-        pkt->data + m_width * m_height,                 // the raw pixel data for the U plane
-        m_width / 2,                                    // the number of bytes between rows of pixel data for the U plane
-        pkt->data + m_width * m_height * 5 / 4,         // the raw pixel data for the V plane
-        m_width / 2                                     // the number of bytes between rows of pixel data for the V plane
+        m_texture,
+        &m_srcRect,
+        pkt->data,
+        m_width,
+        pkt->data + m_width * m_height,
+        m_width / 2,
+        pkt->data + m_width * m_height * 5 / 4,
+        m_width / 2
     );
 
     ret = SDL_RenderClear(m_renderer);
 
-    ret = SDL_RenderCopy(
-        m_renderer,     // the rendering context
-        m_texture,      // the source texture
-        nullptr,           // the source SDL_Rect structure or NULL for the entire texture
-        nullptr            // the destination SDL_Rect structure or NULL for the entire rendering
-                        // target; the texture will be stretched to fill the given rectangle
+    ret = SDL_RenderCopyEx(
+        m_renderer,
+        m_texture,
+        nullptr,
+        nullptr,
+        0,
+        nullptr,
+        m_flip
     );
 
-    // update the screen with any rendering performed since the previous call
     SDL_RenderPresent(m_renderer);
 }
--- core-lib/video/VideoDisplayProcessor.h
+++ core-lib/video/VideoDisplayProcessor.h
@@ -17,10 +17,11 @@ public:
 private:
     void Display(const media_packet_ptr& pkt);
 
-    SDL_Window *m_screen;
+    SDL_Window *m_window;
     SDL_Renderer *m_renderer;
     SDL_Texture *m_texture;
-    SDL_Rect m_rect;
+    SDL_Rect m_srcRect, m_dstRect;
     const int m_width;
     const int m_height;
+    const SDL_RendererFlip m_flip = SDL_RendererFlip::SDL_FLIP_HORIZONTAL;
 };
